d2b8bafab44f8eb13222f40de93af1a8283da8fc
zhoufeiyn <zhoufeiyn@gmail.com>
2025-03-09 23:04:45 -0700

    add student hub urls in urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),  # Root URL
     path('login/', views.login_view, name='login'),
+    path('chapter1/',views.chapter1,name='chapter1'),
+    path('chapter2/',views.chapter2,name='chapter2'),
+    path('chapter3/', views.chapter3, name='chapter3'),
+    path('studenthub/',views.studenthub,name='studenthub'),
 
 ]
 
