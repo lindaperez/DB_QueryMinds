af8f026c1f060cd166d64f38109b328b3826ec87
zhoufeiyn <zhoufeiyn@gmail.com>
2025-03-09 23:07:11 -0700

    add student hub view basic functions in views.py

--- myapp/views.py
+++ myapp/views.py
@@ -21,3 +21,16 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
+
+
+def chapter1(request):
+    return render(request,'chapter1.html')
+
+def chapter2(request):
+    return render(request,'chapter2.html')
+
+def chapter3(request):
+    return render(request,'chapter3.html')
+
+def studenthub(request):
+    return render(request,'studenthub.html')
